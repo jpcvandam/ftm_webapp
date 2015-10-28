73c51d22214cc2a7be7ac43409fc3dd41ce3da17
jpcvandam <jpcvandam@gmail.com>
2015-10-28 09:12:16 +0100

    database_fill.py added, its now under construction

---
+++ database_fill.py
@@ -0,0 +1,4 @@
+#database filler and reader
+#purpose: fill and maintain a meteodatabase for the FTM and make fast data searching and slicing in dateranges possible
+#Author: John van Dam
+#Date: 28 October 2015
