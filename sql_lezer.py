3ec726c162cb9f1301b55d1abbc42b4be0d47611
jpcvandam <jpcvandam@gmail.com>
2015-11-02 12:02:47 +0100

    sql_lezer werkt moet het nu gaan doen met de Django utilities, hij doet wel iets, maar werkt niet 100%

--- sql_lezer.py
+++ sql_lezer.py
@@ -2,19 +2,33 @@
 #auteur: John van Dam
 #datum: 02-11-2015
 
-from django.db import connection
+from django.db import connection, models
 from dateutil import parser
+from knmi_database.models import MeteoData
+import numpy as np
+from datetime import datetime
+
 
 
 def meteo_lezen(nummer_station, startdatum, einddatum):
 	cursor = connection.cursor()
 	startdatum = parser.parse(startdatum)
 	einddatum = parser.parse(einddatum)
-	print startdatum, einddatum
+	#print startdatum, einddatum
 	sql = """SELECT * FROM  knmi_database_meteodata WHERE datum >=\" """ + str(startdatum) +  """\" AND datum<= \"""" + str(einddatum) + """\" AND nummer= """ + str(nummer_station)+";"
 	#sql="""SELECT * FROM  knmi_database_meteodata WHERE datum >= "1992-01-05 00:00:00" AND datum<= "1992-01-14 00:00:00" AND nummer= 344;"""
-	print sql
+	#print sql
 	cursor.execute(sql)
 	rows = cursor.fetchall()
 	print rows
+
+
+def meteo_query(nummerstation, startdatum, einddatum):
+	startdatum = parser.parse(startdatum)
+	einddatum = parser.parse(einddatum)
+	qs = MeteoData.objects.get(datum__gt=datetime.date(startdatum), nummer__gt=nummerstation) 
+	#.get haalt in dit geval teveel op en dat moet ook, maar mag niet van Django, datum__gt werkt in elk geval en daar was het om te doen
+	vlqs - qs.values_list()
+	rij = np.core.records.fromrecords (vlqs, names=[f.name for f in MeteoData._meta.fields])
+	print rij
 	
