f9ebf3010825d1d3eac445ca0d708e5481e043a8
jpcvandam <jpcvandam@gmail.com>
2015-11-05 16:52:55 +0100

    sql_lezer werkt nu, het moment dat data uit de query omgezet kan worden naar een int is nabij

--- sql_lezer.py
+++ sql_lezer.py
@@ -30,5 +30,8 @@ def meteo_query(nummerstation, startdatum, einddatum):
 	q = q.filter(datum__gte=startdatum)
 	q = q.filter(datum__lte=einddatum)
 	print(q)
+	datumlijst = q.values_list('datum', flat=True)
+	neerslag = values_list('rh', flat=True)
+	verdamping = q.values_list('ev24', flat=True)
 	print 'hoi' #str(rij)
 	
