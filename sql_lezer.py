34b2da39dd75a063b9a55fe85ba06ca1e6b86bf3
jpcvandam <jpcvandam@gmail.com>
2015-11-09 15:03:44 +0100

    update sql_lezer.py

--- sql_lezer.py
+++ sql_lezer.py
@@ -10,18 +10,6 @@ from datetime import datetime
 
 
 
-def meteo_lezen(nummer_station, startdatum, einddatum):
-	cursor = connection.cursor()
-	startdatum = parser.parse(startdatum)
-	einddatum = parser.parse(einddatum)
-	#print startdatum, einddatum
-	sql = """SELECT * FROM  knmi_database_meteodata WHERE datum >=\" """ + str(startdatum) +  """\" AND datum<= \"""" + str(einddatum) + """\" AND nummer= """ + str(nummer_station)+";"
-	#sql="""SELECT * FROM  knmi_database_meteodata WHERE datum >= "1992-01-05 00:00:00" AND datum<= "1992-01-14 00:00:00" AND nummer= 344;"""
-	#print sql
-	cursor.execute(sql)
-	rows = cursor.fetchall()
-	print rows
-
 
 def meteo_query(nummerstation, startdatum, einddatum):
 	startdatum = parser.parse(startdatum)
@@ -32,5 +20,6 @@ def meteo_query(nummerstation, startdatum, einddatum):
 	datumlijst = q.values_list('datum', flat=True)
 	neerslag = q.values_list('rh', flat=True)
 	verdamping = q.values_list('ev24', flat=True)
+	print datumlijst, neerslag, verdamping
 	return datumlijst, neerslag, verdamping
 	
