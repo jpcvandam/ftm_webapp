256364ed2b13bfee0e95086a95cf5ba2a64965a0
jpcvandam <jpcvandam@gmail.com>
2015-11-13 11:47:14 +0100

    update

--- datagrab_parse_knmi.py
+++ datagrab_parse_knmi.py
@@ -93,11 +93,11 @@ for i in [127 ,  32 ,  206 ,  331 ,  200 ,  18 ,  125 ,  16 ,  609 ,  272 ,  161
     meteobestand.close()
 
 #In deze loop zijn alleen de weerstations opgenomen waarvan daadwerkelijk data opgehaald is, het script 'datagrab_knmi.py' haalt voor alle bekende stations data op, maar het merendeel blijkt alleen een header te bevatten met coordinaten en een legenda. Vervolgens zijn er echter niet altijd metingen voor neerslag en verdamping, of er wel andere gegevens op die stations zijn weet ik niet; heb ik niet gebrobeerd, omdat die voor dit project toch nutteloos zijn.
-for i in [215, 225, 235, 240, 242, 249, 251, 257, 260, 265, 267, 269, 270, 273, 275, 277, 278, 279, 280, 283, 286, 290, 310, 323,  319, 330, 340, 344, 348, 350, 356, 370, 375, 377, 380, 391,  ]:
-    meteobestand_in = 'METEO'+str(i)+'.TXT'
-    dfMeteo=pd.read_csv(meteobestand_in, header=None, skiprows=17, skipinitialspace=True, index_col=1, delimiter =',', names = ['STN','YYYYMMDD', 'DR', 'RH', 'RHX', 'RHXH', 'EV24 = DR', 'RH', 'EV24'], parse_dates=[1])
+#for i in [215, 225, 235, 240, 242, 249, 251, 257, 260, 265, 267, 269, 270, 273, 275, 277, 278, 279, 280, 283, 286, 290, 310, 323,  319, 330, 340, 344, 348, 350, 356, 370, 375, 377, 380, 391,  ]:
+#    meteobestand_in = 'METEO'+str(i)+'.TXT'
+#    dfMeteo=pd.read_csv(meteobestand_in, header=None, skiprows=17, skipinitialspace=True, index_col=1, delimiter =',', names = ['STN','YYYYMMDD', 'DR', 'RH', 'RHX', 'RHXH', 'EV24 = DR', 'RH', 'EV24'], parse_dates=[1])
 
-    dfmeteo_out=dfMeteo.RH.sub(dfMeteo.EV24, axis='index')
+#    dfmeteo_out=dfMeteo.RH.sub(dfMeteo.EV24, axis='index')
 
-    meteobestand_uit = 'Waterbalans_METEO'+str(i)+'.csv'
-    appendDFToCSV_void(dfmeteo_out, meteobestand_uit, sep=",")
+#    meteobestand_uit = 'Waterbalans_METEO'+str(i)+'.csv'
+#    appendDFToCSV_void(dfmeteo_out, meteobestand_uit, sep=",")
