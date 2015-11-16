2662bdc44905696d87aefd007f8ce41c253b53d8
jpcvandam <jpcvandam@gmail.com>
2015-11-16 10:45:58 +0100

    logging functie aan het shellscript toegevoegd

--- database_import_geg.sh
+++ database_import_geg.sh
@@ -1,4 +1,6 @@
  #!bin/bash
 source ~/ftm/bin/activate
 cd ~/ftm/ftm
+date >> logs/sql.log
 python manage.py shell < knmi_database/importer.py
+date >> logs/sql.log
--- knmi_grab.sh
+++ knmi_grab.sh
@@ -1,6 +1,9 @@
  #!bin/bash
+date >> ~/ftm/ftm/logs/vul_log.log
 source ~/ftm/bin/activate
 cd ~/ftm/ftm/ftm
 python datagrab_parse_knmi.py
+date >> ~/ftm/ftm/logs/vul_log.log
 cd ~/ftm/ftm
 python manage.py shell < knmi_database/importer.py
+date >> ~/ftm/ftm/logs/vul_log.log
