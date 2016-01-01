2ca05f5760a03f2781842e6ba891079da7815beb
jpcvandam <jpcvandam@gmail.com>
2016-01-01 11:35:05 +0100

    eerste regel shellscript klopt en zorgt dat bij uitvoering van crontab gebruik gemaakt wordt van de bourne again shell

--- knmi_grab.sh
+++ knmi_grab.sh
@@ -1,9 +1,9 @@
- #!bin/bash
+ #!/bin/bash
 date >> ~/ftm/ftm/logs/vul_log.log
 source ~/ftm/bin/activate
 cd ~/ftm/ftm/ftm
 python datagrab_parse_knmi.py
 date >> ~/ftm/ftm/logs/vul_log.log
 cd ~/ftm/ftm
-python manage.py shell < knmi_database/importer.py
+python manage.py shell < knmi_database/importer.py 2>> ~/ftm/ftm/logs/vul_log.log
 date >> ~/ftm/ftm/logs/vul_log.log
