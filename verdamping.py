7ccb08a476090b43bca12817ad660f41cf3e28f3
jpcvandam <jpcvandam@gmail.com>
2016-01-25 13:48:18 +0100

    module bijna af, nu nog testen

--- verdamping.py
+++ verdamping.py
@@ -4,11 +4,6 @@
 
 import numpy as np
 
-def ET0(DELTA, Rn, G, GAMMA, T, e_s, e_a, u2): #verdamping volgens Pennman-Montheit
-    ET0=(0.408*DELTA*(Rn - G) + GAMMA * (900.0/(T + 273.0)) * u2 * (e_s - e_a))/(DELTA + GAMMA *(1.0 + 0.34* u2))
-    return(ET0)
-
-
 def u2(uz, z): #de windsnelheid op 2 m boven maaiveld als functie van de windsnelheid op een andere hoogte en die hoogte, met uz in [m/s] en z in [m]
     u2 = uz * (4.87/(np.log(67.8*z - 5.42)))
     return(u2)
@@ -24,7 +19,32 @@ def eT(T): #dampspanningskromme als functie van T in [graden C]
     return eT
 
 
-def e_ad(Tdew): #actuele dampspanning als functie van het dauwpunt in [graden C]
+def e_a(Tdew): #actuele dampspanning als functie van het dauwpunt in [graden C]
     e_a = 0.6108 * np.exp((17.27 * Tdew)/(Tdew + 237.3))
     return e_a
+
+
+def e_s(Tmin, Tmax):
+    e_s = (eT(Tmax)-eT(Tmin))/2
+    return e_s
+
+
+def e_aRhminmax (RH_mean, Tmin, Tmax): #actuele dampspanning als functie van gemiddelde luchtvochtigheid [%], maximum en minimum temperatuur in [graden C] 
+    e_a = RH_mean/100.0*((eT(Tmax)-eT(Tmin))/2)
+    return e_a
+
+
+def Rn(dswrfsfc, dlwrfsfc, uswrfsfc, ulwrfsfc):
+    #'dswrfsfc', # surface downward short-wave radiation flux [w/m^2]
+    #'dlwrfsfc', # surface downward long-wave rad. flux [w/m^2]
+    #'uswrfsfc', # surface upward short-wave radiation flux [w/m^2] 
+    #'ulwrfsfc', # surface upward long-wave rad. flux [w/m^2]
+    Rn = dswrfsfc + dlwrfsfc - uswrfsfc - ulwrfsfc
+    return Rn
+
+
+def ET0(dswrfsfc, dlwrfsfc, uswrfsfc, ulwrfsfc, T, Tmin, Tmax, Tdew, uz, z): #verdamping volgens Pennman-Montheit
+    ET0=(0.408*DELTA(T-273.15)*(Rn(dswrfsfc, dlwrfsfc, uswrfsfc, ulwrfsfc) - 0) + 0.066 * (900.0/(T -273.15 + 273.0)) * u2(uz, z) * (e_s(Tmin, Tmax) - e_a(Tdew - 273.15)))/(DELTA(T) + 0.066 *(1.0 + 0.34* u2(uz, z)))
+    return(ET0)
+
 
