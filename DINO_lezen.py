6c42cc295c042eb5c7511e0667a4d4adceb8f731
jpcvandam <jpcvandam@gmail.com>
2016-02-03 17:50:20 +0100

    gemeten en berekend worden nu in een grafiek geplot

--- DINO_lezen.py
+++ DINO_lezen.py
@@ -30,7 +30,7 @@ bestandspad = "/home/john/Documenten/Afstuderen_Acacia_water/Data/dino_gedeelte_
 os.chdir(bestandspad)
 infile = "B07A0112001_1.csv" #bestandsnaam met eventueel een pad ervoor
 
-infiles = ["B03D0016001_1.csv", "B03D0016001_1.csv", "B03D0071001_1.csv", "B03G0090001_1.csv", "B03G0091001_1.csv", "B07A0021001_1.csv", "B07A0108001_1.csv", "B07A0112001_1.csv", "B07A0112002_1.csv", "B07A0120001_1.csv", "B07A0121001_1.csv", "B07A0122001_1.csv", "B07A0124001_1.csv", "B07A0125001_1.csv", "B07A0127001_1.csv", "B07A0128001_1.csv", "B07A0130001_1.csv", "B07A0131001_1.csv", "B07A0133001_1.csv", "B07A0134001_1.csv", "B07A0135001_1.csv", "B07A0136001_1.csv", "B07A0140001_1.csv", "B07A0142001_1.csv", "B07A0147001_1.csv", "B07A0924001_1.csv", "B07A0945001_1.csv", "B07A0945002_1.csv", "B07A0946001_1.csv", "B07A0947001_1.csv", "B07A0948002_1.csv", "B07A0949001_1.csv", "B07A0956001_1.csv", "B07A0957001_1.csv", "B07A0981001_1.csv", "B07A0982001_1.csv", "B07B0019001_1.csv", "B07B0019002_1.csv", "B07B0056001_1.csv", "B07B0119001_1.csv", "B07B0130001_1.csv", "B07B0131001_1.csv", "B07B0133001_1.csv", "B07B0135001_1.csv", "B07B0136001_1.csv", "B07B0137001_1.csv", "B07B0138001_1.csv", "B07B0139001_1.csv", "B07B0141001_1.csv", "B07B1108001_1.csv", "B07B1109001_1.csv", "B07E0029001_1.csv", "B07E0030001_1.csv", "B07E0031001_1.csv", "B07E0042001_1.csv", "B07E0084001_1.csv", "B07E0085001_1.csv", "B07E0087001_1.csv", "B07E0088001_1.csv", "B07E0089001_1.csv", "B07E0092001_1.csv", "B07E0094001_1.csv", "B07E0095001_1.csv", "B07E0097001_1.csv", "B07E0098001_1.csv", "B07E0101001_1.csv", "B07E1137001_1.csv", "B03D0071001_1.csv", "B03G0090001_1.csv", "B03G0091001_1.csv", "B07A0021001_1.csv", "B07A0108001_1.csv", "B07A0112001_1.csv", "B07A0112002_1.csv", "B07A0120001_1.csv", "B07A0121001_1.csv", "B07A0122001_1.csv", "B07A0124001_1.csv", "B07A0125001_1.csv", "B07A0127001_1.csv", "B07A0128001_1.csv", "B07A0130001_1.csv", "B07A0131001_1.csv", "B07A0133001_1.csv", "B07A0134001_1.csv", "B07A0135001_1.csv", "B07A0136001_1.csv", "B07A0140001_1.csv", "B07A0142001_1.csv", "B07A0147001_1.csv", "B07A0924001_1.csv", "B07A0945001_1.csv", "B07A0945002_1.csv", "B07A0946001_1.csv", "B07A0947001_1.csv", "B07A0948002_1.csv", "B07A0949001_1.csv", "B07A0956001_1.csv", "B07A0957001_1.csv", "B07A0981001_1.csv", "B07A0982001_1.csv", "B07B0019001_1.csv", "B07B0019002_1.csv", "B07B0056001_1.csv", "B07B0119001_1.csv", "B07B0130001_1.csv", "B07B0131001_1.csv", "B07B0133001_1.csv", "B07B0135001_1.csv", "B07B0136001_1.csv", "B07B0137001_1.csv", "B07B0138001_1.csv", "B07B0139001_1.csv", "B07B0141001_1.csv", "B07B1108001_1.csv", "B07B1109001_1.csv", "B07E0029001_1.csv", "B07E0030001_1.csv", "B07E0031001_1.csv", "B07E0042001_1.csv", "B07E0084001_1.csv", "B07E0085001_1.csv", "B07E0087001_1.csv", "B07E0088001_1.csv", "B07E0089001_1.csv", "B07E0092001_1.csv", "B07E0094001_1.csv", "B07E0095001_1.csv", "B07E0097001_1.csv", "B07E0098001_1.csv", "B07E0101001_1.csv", "B07E1137001_1.csv"]
+infiles = ["B03D0016001_1.csv", "B03D0016001_1.csv", "B03D0071001_1.csv", "B03G0090001_1.csv", "B03G0091001_1.csv", "B07A0021001_1.csv", "B07A0108001_1.csv", "B07A0112001_1.csv", "B07A0112002_1.csv", "B07A0120001_1.csv", "B07A0121001_1.csv", "B07A0122001_1.csv", "B07A0124001_1.csv", "B07A0125001_1.csv", "B07A0127001_1.csv", "B07A0128001_1.csv", "B07A0130001_1.csv", "B07A0131001_1.csv", "B07A0133001_1.csv", "B07A0134001_1.csv", "B07A0135001_1.csv", "B07A0136001_1.csv", "B07A0140001_1.csv", "B07A0142001_1.csv", "B07A0147001_1.csv", "B07A0924001_1.csv", "B07A0945001_1.csv", "B07A0945002_1.csv", "B07A0946001_1.csv", "B07A0947001_1.csv", "B07A0948002_1.csv", "B07A0949001_1.csv", "B07A0956001_1.csv", "B07A0957001_1.csv", "B07A0981001_1.csv", "B07A0982001_1.csv", "B07B0019001_1.csv", "B07B0019002_1.csv", "B07B0056001_1.csv", "B07B0119001_1.csv", "B07B0130001_1.csv", "B07B0131001_1.csv", "B07B0133001_1.csv", "B07B0135001_1.csv", "B07B0136001_1.csv", "B07B0137001_1.csv", "B07B0138001_1.csv", "B07B0139001_1.csv", "B07B0141001_1.csv", "B07B1108001_1.csv", "B07B1109001_1.csv", "B07E0029001_1.csv", "B07E0030001_1.csv", "B07E0042001_1.csv", "B07E0084001_1.csv", "B07E0085001_1.csv", "B07E0087001_1.csv", "B07E0088001_1.csv", "B07E0089001_1.csv",  "B07E0094001_1.csv", "B07E0095001_1.csv", "B07E0097001_1.csv", "B07E0098001_1.csv", "B07E0101001_1.csv", "B07E1137001_1.csv", "B03D0071001_1.csv", "B03G0090001_1.csv", "B03G0091001_1.csv", "B07A0021001_1.csv", "B07A0108001_1.csv", "B07A0112001_1.csv", "B07A0112002_1.csv", "B07A0120001_1.csv", "B07A0121001_1.csv", "B07A0122001_1.csv", "B07A0124001_1.csv", "B07A0125001_1.csv", "B07A0127001_1.csv", "B07A0128001_1.csv", "B07A0130001_1.csv", "B07A0131001_1.csv", "B07A0133001_1.csv", "B07A0134001_1.csv", "B07A0135001_1.csv", "B07A0136001_1.csv", "B07A0140001_1.csv", "B07A0142001_1.csv", "B07A0147001_1.csv", "B07A0924001_1.csv", "B07A0945001_1.csv", "B07A0945002_1.csv", "B07A0946001_1.csv", "B07A0947001_1.csv", "B07A0948002_1.csv", "B07A0949001_1.csv", "B07A0956001_1.csv", "B07A0957001_1.csv", "B07A0981001_1.csv", "B07A0982001_1.csv", "B07B0019001_1.csv", "B07B0019002_1.csv", "B07B0056001_1.csv", "B07B0119001_1.csv", "B07B0130001_1.csv", "B07B0131001_1.csv", "B07B0133001_1.csv", "B07B0135001_1.csv", "B07B0136001_1.csv", "B07B0137001_1.csv", "B07B0138001_1.csv", "B07B0139001_1.csv", "B07B0141001_1.csv", "B07B1108001_1.csv", "B07B1109001_1.csv", "B07E0029001_1.csv", "B07E0030001_1.csv",  "B07E0042001_1.csv", "B07E0084001_1.csv", "B07E0085001_1.csv", "B07E0087001_1.csv", "B07E0088001_1.csv", "B07E0089001_1.csv", "B07E0094001_1.csv", "B07E0095001_1.csv", "B07E0097001_1.csv", "B07E0098001_1.csv", "B07E0101001_1.csv", "B07E1137001_1.csv", "B07E0031001_1.csv", "B07E0031001_1.csv", "B07E0092001_1.csv", "B07E0092001_1.csv"]
 
 
 
@@ -55,23 +55,41 @@ def ruw_csv(infile):
 def plot(df, bestandspad, plotnaam):
     df.plot()
     plt.xlabel('Tijd')
-    plt.title('Tijdstijghoogtelijn')
+    plt.title('Tijdstijghoogtelijn' + plotnaam)
     pylab.savefig(bestandspad + plotnaam + '.png')#, bbox_inches='tight')
     ax = pylab.gca()
     ax.set_ylabel('$cm-mv$')
     ax.text(2, 6,  plotnaam, fontsize=15)
     pylab.close()
 
+
+def plot2(df, df2, bestandspad, plotnaam):
+    ax = df.plot(label='gemeten')
+    df2.plot(ax=ax, label='berekend') #df2.plot(label='berekend')
+    #df.plot(label='gemeten')
+    plt.xlabel('Tijd')
+    plt.title('Tijdstijghoogtelijn ' + plotnaam[9:])
+    pylab.savefig(bestandspad + plotnaam + '.png')#, bbox_inches='tight')
+    ax = pylab.gca()
+    ax.set_ylabel('$cm-mv$')
+    ax.text(2, 6,  plotnaam, fontsize=15)
+    pylab.close()
+
+
 for i in infiles:
     infile = i
     print i
-    overslaan = ruw_csv(infile) 
+    berekend = "berekend/" + infile[0:8] + ".csv"
+    print berekend
+    overslaan = ruw_csv(infile)
+    print overslaan 
     df = pd.read_csv(infile, skiprows=overslaan,index_col=[2], parse_dates=True, usecols={2,4})
-    print(df)
-
+    #print(df)
+    df2 = pd.read_csv(berekend, index_col=[0], parse_dates=True)
     df.dropna(how='any')
-    print df['Stand (cm t.o.v. MV)']
+    #print df['Stand (cm t.o.v. MV)']
     plot(df*-1, bestandspad, i)
+    plot2(df*-1, df2, bestandspad, berekend)
 
 
                
